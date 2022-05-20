07095a6b0e13dafc40764a05ed48b598cfb633b9
HeiseP <95848120+HeiseP@users.noreply.github.com>
2022-05-20 12:13:33 +0200

    js
    prøver at få det forbandede js til at linke sammen!!!

--- Eksamenprojekt 2 semester/js_filer/peter.js
+++ Eksamenprojekt 2 semester/js_filer/peter.js
@@ -1,4 +1,3 @@
-
 let faq = document.getElementsByClassName("faq-page");
 let i;
 for (i = 0; i < faq.length; i++) {
@@ -14,4 +13,23 @@ for (i = 0; i < faq.length; i++) {
             body.style.display = "block";
         }
     });
+}
+//Get the button:
+mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
