f939247ad3a83562662b3f23ad4e2d27eb46e84a
JannikMadsen <jama37791@edu.ucl.dk>
2022-05-23 11:05:16 +0200

    dds
    ds

--- Eksamenprojekt 2 semester/html_filer/janniks.js
+++ Eksamenprojekt 2 semester/html_filer/janniks.js
@@ -1,10 +1,11 @@
+// function + Array
 let li = document.querySelectorAll(".list ul li");
 let list = document.querySelector(".list");
 
 let element = 0,
   count = 0;
 
-//setInterval () method
+//setInterval () method / (loop er ++)
 setInterval(() => {
 if (element < li.length -1){
  list.style.transform = `translateY(-${(count += 4)}em)`;
