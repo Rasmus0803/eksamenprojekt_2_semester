332c88d307d1010bc1dd83e1ad80ab3e1d9d02b2
JannikMadsen <jama37791@edu.ucl.dk>
2022-05-25 16:24:42 +0200

    Update jannik.js

---
+++ Eksamenprojekt 2 semester/js_filer/jannik.js
@@ -0,0 +1,18 @@
+
+let li = document.querySelectorAll(".list ul li");
+let list = document.querySelector(".list");
+
+let element = 0,
+  count = 0;
+
+//setInterval () method /
+setInterval(() => {
+if (element < li.length -1){
+ list.style.transform = `translateY(-${(count += 4)}em)`;
+ element++;
+} else {
+  element= 0;
+  count = 0;
+  list.style.transform = `translateY(-${(count)}em)`;
+}
+},2000);
