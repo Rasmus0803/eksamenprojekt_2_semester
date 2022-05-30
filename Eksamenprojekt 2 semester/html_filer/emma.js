153c163bb82e0df40dbb00990bc6c41a7c56fd50
emmasalling <emmasalling4@gmail.com>
2022-05-30 22:29:38 +0200

    ændring i class navn

--- Eksamenprojekt 2 semester/html_filer/emma.js
+++ Eksamenprojekt 2 semester/html_filer/emma.js
@@ -3,12 +3,12 @@ console.log("Dropdown activated");
 
 
 function dropDown() {
-    document.getElementById("vælgEmne").classList.toggle("show");
+    document.getElementById("vaelgEmne").classList.toggle("show");
 }
 
 let opretArray = new Array("Subject", "Subscriptions", "Log In", "Support", "Other");
 
-    let dropdown = document.getElementById("vælgEmne");
+    let dropdown = document.getElementById("vaelgEmne");
 
     for (let i = 0; i < opretArray.length; ++i) {
         dropdown[dropdown.length] = new Option(opretArray[i], opretArray[i]);
